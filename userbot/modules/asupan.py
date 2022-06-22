f14cd9abe3a6dc9db9fd637812557a0048f63e49
elieve <bayustiadi@ymail.com>
2022-06-22 19:14:48 +0800

    Update asupan.py

--- userbot/modules/asupan.py
+++ userbot/modules/asupan.py
@@ -15,15 +15,58 @@ from .help import *
 
 
 @Client.on_message(filters.me & filters.command("asupan", cmd))
-async def _(event): xx = await edit_or_reply(event, "`Tunggu Sebentar...`") try: asupannya = [ asupan async for asupan in event.client.iter_messages( "@tedeasupancache", filter=InputMessagesFilterVideo ) ] await event.client.send_file( event.chat_id, file=choice(asupannya), reply_to=event.reply_to_msg_id ) await xx.delete() except Exception: await xx.edit("**Tidak bisa menemukan video asupan.**")
+async def _(event):
+    xx = await edit_or_reply(event, "`Tunggu Sebentar...`")
+    try:
+        asupannya = [
+            asupan
+            async for asupan in event.client.iter_messages(
+                "@tedeasupancache", filter=InputMessagesFilterVideo
+            )
+        ]
+        await event.client.send_file(
+            event.chat_id, file=choice(asupannya), reply_to=event.reply_to_msg_id
+        )
+        await xx.delete()
+    except Exception:
+        await xx.edit("**Tidak bisa menemukan video asupan.**")
 
 
 
 @Client.on_message(filters.me & filters.command("bokep", cmd))
-async def _(event): xx = await edit_or_reply(event, "`sabar ya tod...`") try: bokepmu = [ bokep async for bokep in event.client.iter_messages( "@fakyudurov", filter=InputMessagesFilterVideo ) ] await event.client.send_file( event.chat_id, file=choice(nyoh cok selamat coli), reply_to=event.reply_to_msg_id ) await xx.delete() except Exception: await xx.edit("**gak nemu bokep cok mungkin lo dsuruh tobat.**")
+async def _(event):
+    xx = await edit_or_reply(event, "`Tunggu Sebentar...`")
+    try:
+        asupannya = [
+            bokep
+            async for bokep in event.client.iter_messages(
+                "@fakyudurov", filter=InputMessagesFilterVideo
+            )
+        ]
+        await event.client.send_file(
+            event.chat_id, file=choice(asupannya), reply_to=event.reply_to_msg_id
+        )
+        await xx.delete()
+    except Exception:
+        await xx.edit("**Tidak bisa menemukan video asupan.**")
 
 
 
 
 @Client.on_message(filters.me & filters.command("desah", cmd))
-async def _(event): xx = await edit_or_reply(event, "`sabar ya tod...`") try: desahmu = [ desah async for desah in event.client.iter_messages( "@desahancewesangesange", filter=InputMessagesFilterVoice_Note ) ] await event.client.send_file( event.chat_id, file=choice(nyoh cok selamat coli), reply_to=event.reply_to_msg_id ) await xx.delete() except Exception: await xx.edit("**gak nemu desahan cok mungkin lo dsuruh tobat.**")
+async def _(event):
+    xx = await edit_or_reply(event, "`Tunggu Sebentar...`")
+    try:
+        asupannya = [
+            desahan
+            async for desahan in event.client.iter_messages(
+                "@desahancewesangesange", filter=InputMessagesFilterVoice_Note
+            )
+        ]
+        await event.client.send_file(
+            event.chat_id, file=choice(desahanya), reply_to=event.reply_to_msg_id
+        )
+        await xx.delete()
+    except Exception:
+        await xx.edit("**Tidak bisa menemukan desahann.**")
+     **")
