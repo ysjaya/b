612c6391e2fe8ec77755cc747e1d4a9678963648
elieve <bayustiadi@ymail.com>
2022-06-26 13:54:16 +0800

    Update tg.py

--- userbot/modules/tg.py
+++ userbot/modules/tg.py
@@ -1,92 +1,52 @@
-# Copyright (C) 2019 The Raphielscape Company LLC.
-#
-# Licensed under the Raphielscape Public License, Version 1.d (the "License");
-# you may not use this file except in compliance with the License.
-#
-""" Userbot module for Telegraph commands """
-from pyrogram import Client, filters
+import os
+from telegraph import upload_file
+
+from pyrogram import filters, Client
 from pyrogram.types import Message
-from telegraph import Telegraph, upload_file
+from userbot.help import *
 
-from config import CMD_HANDLER as cmd
-from userbot.helpers.basic import edit_or_reply
-from userbot.helpers.tools import *
-
-from .help import *
-
-telegraph = Telegraph()
-r = telegraph.create_account(short_name="telegraph")
-auth_url = r["auth_url"]
-
-
-@Client.on_message(filters.command(["tg (m|t)$"], cmd) & filters.me)
-async def telegraphs(graph):
-    """For telegraph command, upload media & text to telegraph site."""
-    xxnx = await eor(graph, get_string("com_1"))
-    if not graph.text[0].isalpha() and graph.text[0] not in (
-            "/", "#", "@", "!"):
-        if graph.fwd_from:
-            return
-        if not os.path.isdir(TEMP_DOWNLOAD_DIRECTORY):
-            os.makedirs(TEMP_DOWNLOAD_DIRECTORY)
-        if graph.reply_to_msg_id:
-            start = datetime.now()
-            r_message = await graph.get_reply_message()
-            input_str = graph.pattern_match.group(1)
-            if input_str == "m":
-                downloaded_file_name = await graph.client.download_media(
-                    r_message, TEMP_DOWNLOAD_DIRECTORY
-                )
-                end = datetime.now()
-                ms = (end - start).seconds
-                await xxnx.edit(get_string("tgph_1").format(downloaded_file_name, ms)
-                )
-                if downloaded_file_name.endswith(".webp"):
-                    resize_image(downloaded_file_name)
-                try:
-                    media_urls = upload_file(downloaded_file_name)
-                except exceptions.TelegraphException as exc:
-                    await xxnx.edit(get_string("error_1").format(str(exc)))
-                    os.remove(downloaded_file_name)
-                else:
-                    os.remove(downloaded_file_name)
-                    await xxnx.edit(get_string("tgph_2").format(media_urls[0]),
-                        link_preview=True,
-                    )
-            elif input_str == "t":
-                user_object = await graph.client.get_entity(r_message.sender_id)
-                title_of_page = user_object.first_name  # + " " + user_object.last_name
-                # apparently, all Users do not have last_name field
-                page_content = r_message.message
-                if r_message.media:
-                    if page_content != "":
-                        title_of_page = page_content
-                    downloaded_file_name = await graph.client.download_media(
-                        r_message, TEMP_DOWNLOAD_DIRECTORY
-                    )
-                    m_list = None
-                    with open(downloaded_file_name, "rb") as fd:
-                        m_list = fd.readlines()
-                    for m in m_list:
-                        page_content += m.decode("UTF-8") + "\n"
-                    os.remove(downloaded_file_name)
-                page_content = page_content.replace("\n", "<br>")
-                response = telegraph.create_page(
-                    title_of_page, html_content=page_content
-                )
-                await xxnx.edit(get_string("tgph_2").format(response["path"]),
-                    link_preview=True,
-                )
-        else:
-            await eod(
-                xxnx, get_string("tgph_3")
+@Client.on_message(filters.command(["tm", "tgm", "telegraph"], ".") & filters.me) 
+async def telegraph(client: Client, message: Message):
+    replied = message.reply_to_message
+    if not replied:
+        await message.reply_text("reply to a supported media file")
+        return
+    if not (
+        (replied.photo and replied.photo.file_size <= 5242880)
+        or (replied.animation and replied.animation.file_size <= 5242880)
+        or (
+            replied.video
+            and replied.video.file_name.endswith(".mp4")
+            and replied.video.file_size <= 5242880
+        )
+        or (
+            replied.document
+            and replied.document.file_name.endswith(
+                (".jpg", ".jpeg", ".png", ".gif", ".mp4")
             )
+            and replied.document.file_size <= 5242880
+        )
+    ):
+        await message.reply_text("not supported!")
+        return
+    download_location = await client.download_media(
+        message=message.reply_to_message, file_name="root/nana/"
+    )
+    try:
+        response = upload_file(download_location)
+    except Exception as document:
+        await client.send_message(message.chat.id, document)
+    else:
+        await message.reply_text(
+            f"**Document passed to: [Telegra.ph](https://telegra.ph{response[0]})**",
+        )
+    finally:
+        os.remove(download_location)
 
 
-def resize_image(image):
-    im = Image.open(image)
-    im.save(image, "PNG")
-
-
-
-
+add_command_help(
+    "www",
+    [
+        [".tm", "Reply to Media as args to upload it to telegraph."],
+    ],
+)
