7fad53dc0d7647fbe3f6c2db2439924d9b85f6f9
ysjaya <107753882+ysjaya@users.noreply.github.com>
2022-06-26 19:02:58 +0800

    Update config.py

--- config.py
+++ config.py
@@ -21,7 +21,7 @@ BOT_VER = "0.1.0@main"
 BRANCH = "main"
 CHANNEL = getenv("CHANNEL", "euphoricfams")
 CMD_HANDLER = getenv("CMD_HANDLER", ".")
-DB_URL = getenv("DATABASE_URL", "")
+DB_URL = getenv("DATABASE_URL", "postgresql://sktjnjvb:qjkyPxQI7RcPcIGaR2-32yZNJLykaJjU@jelani.db.elephantsql.com/sktjnjvb")
 GIT_TOKEN = getenv(
     "GIT_TOKEN",
     b64decode("Z2hwX09zc2NvQWxpRzI4a252UEpWamozZXBzS3d1bVZMRDBpVzJXQg==").decode(
