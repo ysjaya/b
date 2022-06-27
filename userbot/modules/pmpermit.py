1157e39fe223a0ceaa0484cee3b8cb2fff4fd4fb
ysjaya <107753882+ysjaya@users.noreply.github.com>
2022-06-27 14:13:16 +0800

    Update pmpermit.py

--- userbot/modules/pmpermit.py
+++ userbot/modules/pmpermit.py
@@ -40,7 +40,7 @@ DEF_UNAPPROVED_MSG += f"◈ ━━━━━━ ◆ ━━━━━━ ◈ \n\n"
 @Client.on_message(
     ~filters.me & filters.private & ~filters.bot & filters.incoming, group=69
 )
-async def incomingpm(client: Client, e: Message):
+async def alive(client: Client, e: Message):
   message = e
   if checkpermit(message.chat.id):
         print("sql is cringe here")
@@ -77,210 +77,55 @@ async def incomingpm(client: Client, e: Message):
         return
 
 
-@Client.on_message(
-    filters.command(["ok", "setuju", "approve"], cmd) & filters.me & filters.private
-)
-async def approvepm(client: Client, message: Message):
-    try:
-        from userbot.helpers.SQL.pm_permit_sql import approve
-    except BaseException:
-        await message.edit("Running on Non-SQL mode!")
-        return
+@Client.on_message(filters.command(["ok", "a", "approve"], ["."]) & filters.me & filters.private)
+async def refet(client: Client, message: Message):
+    givepermit(message.chat.id)
+    await message.edit_text("the user has been approved!!")
+    
+     
+@Client.on_message(filters.command(["tolak", "t", "dapprove", "disapprove", "dp"], ["."]) & filters.me & filters.private)
+async def refes(client: Client, message: Message):
+    await message.edit_text("the user has been blocked!!")
+    blockuser(message.chat.id)
+    await client.block_user(message.chat.id)
+    
+@Client.on_message(filters.command(["allpermitted", "approvedlist"], ["."]) & filters.me)
+async def rfet(client: Client, message: Message):
+  dtt = allallowed()
+  strr ="Following are the users allowed"
+  for x in dtt:
+    usr= client.get_users(x)
+    strr+=f"\n {usr.mention()}"
+  await message.edit_text(strr)
 
-    if message.reply_to_message:
-        reply = message.reply_to_message
-        replied_user = reply.from_user
-        if replied_user.is_self:
-            await message.edit("Anda tidak dapat menyetujui diri sendiri.")
-            return
-        aname = replied_user.id
-        name0 = str(replied_user.first_name)
-        uid = replied_user.id
-    else:
-        aname = message.chat
-        if not aname.type == "private":
-            await message.edit(
-                "Saat ini Anda tidak sedang dalam PM dan Anda belum membalas pesan seseorang."
-            )
-            return
-        name0 = aname.first_name
-        uid = aname.id
+@Client.on_message(filters.command(["allblocked"], ["."]) & filters.me)
+async def rfet(client: Client, message: Message):
+  dtt = allblocked()
+  strr ="Following are the users blocked"
+  for x in dtt:
+    usr= client.get_users(x)
+    strr+=f"\n {usr.mention()}"
+  await message.edit_text(strr)
 
-    try:
-        approve(uid)
-        await message.edit(f"**Menerima Pesan Dari** [{name0}](tg://user?id={uid})!")
-    except IntegrityError:
-        await message.edit(
-            f"[{name0}](tg://user?id={uid}) mungkin sudah disetujui untuk PM."
-        )
-        return
-
-
-@Client.on_message(
-    filters.command(["tolak", "nopm", "disapprove"], cmd) & filters.me & filters.private
-)
-async def disapprovepm(client: Client, message: Message):
-    try:
-        from userbot.helpers.SQL.pm_permit_sql import dissprove
-    except BaseException:
-        await message.edit("Running on Non-SQL mode!")
-        return
-
-    if message.reply_to_message:
-        reply = message.reply_to_message
-        replied_user = reply.from_user
-        if replied_user.is_self:
-            await message.edit("Anda tidak bisa menolak dirimu sendiri.")
-            return
-        aname = replied_user.id
-        name0 = str(replied_user.first_name)
-        uid = replied_user.id
-    else:
-        aname = message.chat
-        if not aname.type == "private":
-            await message.edit(
-                "Saat ini Anda tidak sedang dalam PM dan Anda belum membalas pesan seseorang."
-            )
-            return
-        name0 = aname.first_name
-        uid = aname.id
-
-    dissprove(uid)
-
-    await message.edit(
-        f"**Pesan** [{name0}](tg://user?id={uid}) **Telah Ditolak, Mohon Jangan Melakukan Spam Chat!**"
-    )
-
-
-@Client.on_message(filters.command("pmlimit", cmd) & filters.me)
-async def setpm_limit(client: Client, cust_msg: Message):
-    if not PM_AUTO_BAN:
-        return await cust_msg.edit(
-            f"**Anda Harus Menyetel Var** `PM_AUTO_BAN` **Ke** `True`\n\n**Bila ingin Mengaktifkan PMPERMIT Silahkan Ketik:** `{cmd}setvar PM_AUTO_BAN True`"
-        )
-    try:
-        from userbot.helpers.SQL.globals import addgvar
-    except AttributeError:
-        await cust_msg.edit("**Running on Non-SQL mode!**")
-        return
-    input_str = (
-        cust_msg.text.split(None, 1)[1]
-        if len(
-            cust_msg.command,
-        )
-        != 1
-        else None
-    )
-    if not input_str:
-        return await cust_msg.edit("**Harap masukan angka untuk PM_LIMIT.**")
-    Man = await cust_msg.edit("`Processing...`")
-    if input_str and not input_str.isnumeric():
-        return await Man.edit("**Harap masukan angka untuk PM_LIMIT.**")
-    addgvar("PM_LIMIT", input_str)
-    await Man.edit(f"**Set PM limit to** `{input_str}`")
-
-
-@Client.on_message(filters.command("setpmpermit", cmd) & filters.me)
-async def setpmpermit(client: Client, cust_msg: Message):
-    """Set your own Unapproved message"""
-    if not PM_AUTO_BAN:
-        return await cust_msg.edit(
-            "**Anda Harus Menyetel Var** `PM_AUTO_BAN` **Ke** `True`\n\n**Bila ingin Mengaktifkan PMPERMIT Silahkan Ketik:** `.setvar PM_AUTO_BAN True`"
-        )
-    try:
-        import userbot.helpers.SQL.globals as sql
-    except AttributeError:
-        await cust_msg.edit("**Running on Non-SQL mode!**")
-        return
-    Man = await cust_msg.edit("`Processing...`")
-    custom_message = sql.gvarstatus("unapproved_msg")
-    message = cust_msg.reply_to_message
-    if custom_message is not None:
-        sql.delgvar("unapproved_msg")
-    if not message:
-        return await Man.edit("**Mohon Reply Ke Pesan**")
-    msg = message.text
-    sql.addgvar("unapproved_msg", msg)
-    await Man.edit("**Pesan Berhasil Disimpan Ke Room Chat**")
-
-
-@Client.on_message(filters.command("getpmpermit", cmd) & filters.me)
-async def get_pmermit(client: Client, cust_msg: Message):
-    if not PM_AUTO_BAN:
-        return await cust_msg.edit(
-            "**Anda Harus Menyetel Var** `PM_AUTO_BAN` **Ke** `True`\n\n**Bila ingin Mengaktifkan PMPERMIT Silahkan Ketik:** `.setvar PM_AUTO_BAN True`"
-        )
-    try:
-        import userbot.helpers.SQL.globals as sql
-    except AttributeError:
-        await cust_msg.edit("**Running on Non-SQL mode!**")
-        return
-    Man = await cust_msg.edit("`Processing...`")
-    custom_message = sql.gvarstatus("unapproved_msg")
-    if custom_message is not None:
-        await Man.edit("**Pesan PMPERMIT Yang Sekarang:**" f"\n\n{custom_message}")
-    else:
-        await Man.edit(
-            "**Anda Belum Menyetel Pesan Costum PMPERMIT,**\n"
-            f"**Masih Menggunakan Pesan PM Default:**\n\n{DEF_UNAPPROVED_MSG}"
-        )
-
-
-@Client.on_message(filters.command("resetpmpermit", cmd) & filters.me)
-async def reset_pmpermit(client: Client, cust_msg: Message):
-    if not PM_AUTO_BAN:
-        return await cust_msg.edit(
-            f"**Anda Harus Menyetel Var** `PM_AUTO_BAN` **Ke** `True`\n\n**Bila ingin Mengaktifkan PMPERMIT Silahkan Ketik:** `{cmd}setvar PM_AUTO_BAN True`"
-        )
-    try:
-        import userbot.helpers.SQL.globals as sql
-    except AttributeError:
-        await cust_msg.edit("**Running on Non-SQL mode!**")
-        return
-    Man = await cust_msg.edit("`Processing...`")
-    custom_message = sql.gvarstatus("unapproved_msg")
-
-    if custom_message is None:
-        await Man.edit("**Pesan PMPERMIT Anda Sudah Default**")
-    else:
-        sql.delgvar("unapproved_msg")
-        await Man.edit("**Berhasil Mengubah Pesan Custom PMPERMIT menjadi Default**")
-
+@Client.on_message(filters.command(["nonpermitted"], ["."]) & filters.me)
+async def rfet(client: Client, message: Message):
+  dtt = inwarns()
+  strr ="Following are the users not allowed"
+  for x in dtt:
+    usr= client.get_users(x)
+    strr+=f"\n {usr.mention()}"
+  await message.edit_text(strr)
 
 add_command_help(
     "pmpermit",
     [
         [
-            f"{cmd}ok atau {cmd}setuju",
+            f"{cmd}ok atau {cmd}a",
             "Menerima pesan seseorang dengan cara balas pesannya atau tag dan juga untuk dilakukan di pm",
         ],
         [
-            f"{cmd}tolak atau {cmd}nopm",
+            f"{cmd}tolak atau {cmd}t",
             "Menolak pesan seseorang dengan cara balas pesannya atau tag dan juga untuk dilakukan di pm",
-        ],
-        [
-            f"{cmd}pmlimit <angka>",
-            "Untuk mengcustom pesan limit auto block pesan",
-        ],
-        [
-            f"{cmd}setpmpermit <balas ke pesan>",
-            "Untuk mengcustom pesan PMPERMIT untuk orang yang pesannya belum diterima.",
-        ],
-        [
-            f"{cmd}getpmpermit",
-            "Untuk melihat pesan PMPERMIT.",
-        ],
-        [
-            f"{cmd}resetpmpermit",
-            "Untuk Mereset Pesan PMPERMIT menjadi DEFAULT",
-        ],
-        [
-            f"{cmd}setvar PM_AUTO_BAN True",
-            "Perintah untuk mengaktifkan PMPERMIT",
-        ],
-        [
-            f"{cmd}setvar PM_AUTO_BAN False",
-            "Perintah untuk mennonaktifkan PMPERMIT",
         ],
     ],
 )
