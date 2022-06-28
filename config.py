51c3a061462684a5433faed27f6859175ddc77b3
ysjaya <107753882+ysjaya@users.noreply.github.com>
2022-06-28 20:17:22 +0800

    Update config.py

--- config.py
+++ config.py
@@ -22,12 +22,7 @@ BRANCH = "main"
 CHANNEL = getenv("CHANNEL", "euphoricfams")
 CMD_HANDLER = getenv("CMD_HANDLER", ".")
 DB_URL = getenv("DATABASE_URL", "")
-GIT_TOKEN = getenv(
-    "GIT_TOKEN",
-    b64decode("Z2hwX09zc2NvQWxpRzI4a252UEpWamozZXBzS3d1bVZMRDBpVzJXQg==").decode(
-        "utf-8"
-    ),
-)
+
 
 GROUP = getenv("GROUP", "euphoricfams")
 HEROKU_API_KEY = getenv("HEROKU_API_KEY", "")
@@ -36,7 +31,7 @@ PMPERMIT_PIC = getenv("PMPERMIT_PIC", "https://telegra.ph/file/5e82760d6ef347713
 PM_AUTO_BAN = strtobool(getenv("PM_AUTO_BAN", "True"))
 REPO_URL = getenv(
     "REPO_URL",
-    b64decode("aHR0cHM6Ly9naXRodWIuY29tL2VsaWV2ZS90ZXM=").decode("utf-8"),
+    b64decode("aHR0cHM6Ly9naXRodWIuY29tL3lzamF5YS9i").decode("utf-8"),
 )
 
 STRING_SESSION1 = getenv("STRING_SESSION1", "")
