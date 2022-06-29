7c644388ead60db6c04182f68332fa0e6bf1bd38
elieve <bayustiadi@ymail.com>
2022-06-29 19:58:42 +0800

    Update tes.py

--- userbot/modules/tes.py
+++ userbot/modules/tes.py
@@ -1,19 +1,14 @@
 from pyrogram.types import (InlineKeyboardButton, InlineKeyboardMarkup, Message)
 from pyrogram import Client, filters 
 from config import CMD_HANDLER as cmd 
-
+import asyncio
 
 @Client.on_message(filters.command(["help1"], cmd) & filters.me)
-async def help1(client: Client, message: Message):
-  await message.reply_text(
-      text="BayUserBot Modules",
-      reply_markup=InlineKeyboardMarkup
-      (
-        [
-          [
-            InlineKeyboardButton("afk", url="t.me/euphoricfams"),
-          ]
-        ]
-      )
-    )
-
+async def _create_keyboard_button(client, message):
+        if isinstance(message, tuple):
+            if 'http' in message[1]:
+                return InlineKeyboardButton(text=tes[0], url=1[1])
+            else:
+                return InlineKeyboardButton(text=tes[0], callback_data=1[1])
+        else:
+            return InlineKeyboardButton(text=tes, callback_data=1)
